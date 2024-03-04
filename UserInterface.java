0e2208fd0e529b7644ae25c9587c1ef88db67643
malthe-kea <mati0002@stud.kea.dk>
2024-03-04 13:34:49 +0100

    Fik UserInterface.java til at virke.

--- UserInterface.java
+++ UserInterface.java
@@ -1,34 +1,43 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInterface {
-    public static void main(String[] args) {
+
+    public UserInterface() {
+
         System.out.println("Welcome to this magical journey");
         System.out.println("Write 'go north' to go north");
         System.out.println("Write 'go west' to go west");
         System.out.println("Write 'go south' to go south");
         System.out.println("Write 'go east' to go east");
+        System.out.println("Write 'exit' to exit to the main menu");
+        System.out.println("Write 'help' to open the help menu");
+
 
         Scanner input = new Scanner(System.in);
-        //Scanne-objekt.
-
         String userInput = input.nextLine();
         input.useDelimiter("\n");
         switch (userInput.toLowerCase()) {
             case "go north":
                 System.out.println("going North");
-            break;
-            case  "go west":
+                break;
+            case "go west":
                 System.out.println("going west");
-            break;
+                break;
             case "go south":
                 System.out.println("Going south");
-            break;
+                break;
             case "go east":
                 System.out.println("Going East");
-            break;
+                break;
+            case "exit":
+                System.exit(0);
+                break;
+            case "help":
+                System.out.println("***Under construction***");
+                break;
             default:
-                System.out.println("You cannot go that way");
-
+                System.out.println("You cannot go that way, try again");
         }
     }
 }

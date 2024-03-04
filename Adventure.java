9b0e6aa9a6628ef7166526366fbc4177b2f61295
alna002 <alna0002@stud.kea.dk>
2024-03-04 15:54:04 +0100

    Rooms og deres beskrivelser.

--- Adventure.java
+++ Adventure.java
@@ -1,5 +1,5 @@
 public class Adventure {
-
+    //opret et enkelt room-objekt i Adventure. Lad det være currentRoom, som spilleren befinder sig i. Udskriv navn og beskrivelse når spilleren skriver "look".
     Room currentRoom = new Room("'THIS IS ROOM 1'",
             "You find yourself in a dimly lit tavern, \n" +
                     "the air thick with the scent of ale and wood smoke. \n" +
@@ -7,9 +7,44 @@ public class Adventure {
 
     public void Adventure() {
     }
-    public String look () {
+
+    public String look() {
         String look = (currentRoom.getRoomName() + currentRoom.getRoomDiscription());
         return look;
     }
-}
 
+    // Opret de otte andre room-objekter, med navne og beskrivelser, og forbind dem
+
+    public void CreateRoom() {
+        Room room1 = new Room(" room1", "");
+        Room room2 = new Room(" room2", "");
+        Room room3 = new Room("room3", "");
+        Room room4 = new Room("room4", "");
+        Room room5 = new Room("room5", "");
+        Room room6 = new Room("room6", "");
+        Room room7 = new Room("room7", "");
+        Room room8 = new Room("room8", "");
+        Room room9 = new Room("room9", "");
+
+
+        //Romms skal være forbundet til hinanden som på billedet
+        room1.setEast(room2);
+        room2.setWest(room1);
+        room2.setEast(room3);
+        room3.setWest(room2);
+        room3.setSouth(room6);
+        room4.setNorth(room1);
+        room4.setSouth(room7);
+        room5.setSouth(room8);
+        room6.setNorth(room3);
+        room6.setSouth(room9);
+        room7.setNorth(room4);
+        room7.setEast(room8);
+        room8.setWest(room7);
+        room8.setNorth(room5);
+        room8.setEast(room9);
+        room8.setWest(room7);
+        room9.setWest(room8);
+        room9.setNorth(room6);
+    }
+}

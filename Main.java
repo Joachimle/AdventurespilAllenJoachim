92faa60f911ff5d141c07e4487adc6fd5520eb06
malthe-kea <mati0002@stud.kea.dk>
2024-03-04 15:13:57 +0100

    Med hjælp fra en kollega har jeg nu fået lavet UserInterface sådan, at det ikke crasher, hvis man taster noget forkert.

--- Main.java
+++ Main.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         UserInterface startGame = new UserInterface();
-        System.out.println(startGame);
+        System.out.println("Welcome");
+        startGame.userOptionsForDirections();
+
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            String userInput = input.nextLine();
+            input.useDelimiter("\n");
+            boolean result = startGame.processUserInput(userInput);
+            if (result) {
+                //user input OK.
+            } else {
+                //User input not OK; retry
+            }
+            startGame.userOptionsForDirections();
+        }
     }
 }

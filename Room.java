07e0dc1287cf359e4deb4b102e2af3f4acadb635
alna002 <alna0002@stud.kea.dk>
2024-03-04 11:43:57 +0100

    Room klassen med Attributter og set+get methods

--- Room.java
+++ Room.java
@@ -1,7 +1,61 @@
 public class Room {
     //attributes
+    private String roomName;
+    private String roomDiscription;
+    private Room north, east, west, south;  // room objekt
 
     //Constructor
-    public Room (){
+    public Room(String roomName, String roomDiscription) {
+        this.roomDiscription = roomDiscription;
+        this.roomName = roomName;
+
+    }
+    // Getter Methods
+
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public String getRoomDiscription() {
+        return roomDiscription;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public void setEast(Room east) {
+        this.east = east;
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+    }
+
+    public void setNorth(Room north) {
+        this.north = north;
+    }
+
+    public void setSouth(Room south) {
+        this.south = south;
     }
 }
+
+
+/*public String toString (){
+    String direction = "";
+    direction*/
+

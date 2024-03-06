ad8655b6a26bfbd621f86de671603eb21b59708c
malthe-kea <mati0002@stud.kea.dk>
2024-03-06 13:33:04 +0100

    *KODEN VIRKER IKKE I DENNE VERSION*
    Playerklassen er blevet opdateret med udkommenteret kode, der skal laves til en switch case, så den kan kaldes i UserInterface-klassen.

--- Player.java
+++ Player.java
@@ -15,3 +15,41 @@ public class Player {
     }
 
 }
+
+/*
+case "go north":
+        if (player.map.currentRoom.getNorth() != null) {
+        System.out.println("please write a direction");
+player.map.currentRoom = player.map.currentRoom.getNorth();
+                        System.out.println("Going North");
+                    } else {
+                            System.out.println("You can't go this way");
+                    }
+                            break;
+                            case "go west":
+                            if (player.map.currentRoom.getWest() != null) {
+        System.out.println("please write a direction");
+player.map.currentRoom = player.map.currentRoom.getWest();
+                        System.out.println("Going North");
+                    } else {
+                            System.out.println("You can't go this way");
+                    }
+                            break;
+                            case "go south":
+                            if (player.map.currentRoom.getSouth() != null) {
+        System.out.println("please write a direction");
+player.map.currentRoom = player.map.currentRoom.getSouth();
+                        System.out.println("going south");
+                    } else {
+                            System.out.println("You can't go this way");
+                    }
+                            break;
+                            case "go east":
+                            if (player.map.currentRoom.getEast() != null) {
+        System.out.println("please write a direction");
+player.map.currentRoom = player.map.currentRoom.getEast();
+                        System.out.println("Going East");
+                    } else {
+                            System.out.println("You can't go this way");
+                    }
+                            break;
--- UserInterface.java
+++ UserInterface.java
@@ -20,42 +20,8 @@ public class UserInterface {
         while (gameIsRunning) {
             command = scanner.next().toLowerCase();
             switch (command) {
-                case "go north":
-                    if (player.map.currentRoom.getNorth() != null) {
-                        System.out.println("please write a direction");
-                        player.map.currentRoom = player.map.currentRoom.getNorth();
-                        System.out.println("Going North");
-                    } else {
-                        System.out.println("You can't go this way");
-                    }
-                    break;
-                case "go west":
-                    if (player.map.currentRoom.getWest() != null) {
-                        System.out.println("please write a direction");
-                        player.map.currentRoom = player.map.currentRoom.getWest();
-                        System.out.println("Going North");
-                    } else {
-                        System.out.println("You can't go this way");
-                    }
-                    break;
-                case "go south":
-                    if (player.map.currentRoom.getSouth() != null) {
-                        System.out.println("please write a direction");
-                        player.map.currentRoom = player.map.currentRoom.getSouth();
-                        System.out.println("going south");
-                    } else {
-                        System.out.println("You can't go this way");
-                    }
-                    break;
-                case "go east":
-                    if (player.map.currentRoom.getEast() != null) {
-                        System.out.println("please write a direction");
-                        player.map.currentRoom = player.map.currentRoom.getEast();
-                        System.out.println("Going East");
-                    } else {
-                        System.out.println("You can't go this way");
-                    }
-                    break;
+                case "go direction":
+                    //her skal playerobjektet tilføjes, når player-klassen er færdig.
                 case "exit":
                     System.exit(0);
                     break;

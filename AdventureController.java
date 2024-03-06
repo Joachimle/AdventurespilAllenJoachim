71feb844aad99a95ee96cb468412a7b67a1a4acb
malthe-kea <mati0002@stud.kea.dk>
2024-03-06 10:12:05 +0100

    Refactoret Adventure-class til AdventureController.

--- AdventureController.java
+++ AdventureController.java
@@ -1,15 +1,11 @@
 public class AdventureController {
     //opret et enkelt room-objekt i Adventure. Lad det være currentRoom, som spilleren befinder sig i. Udskriv navn og beskrivelse når spilleren skriver "look".
     Room currentRoom;
+    //Map createMap = new Map();
+    UserInterface userInterface = new UserInterface();
 
     public AdventureController() {
-        createRoom();
-    }
-
-    public String look() {
-        String look = (currentRoom.getRoomName() + currentRoom.getRoomDiscription());
-        return look;
-    }
-
+        Map createMap = new Map();
+        userInterface.processUserInput();
     }
 }
--- Main.java
+++ Main.java
@@ -4,23 +4,6 @@ public class Main {
     public static void main(String[] args) {
         AdventureController startGame = new AdventureController();
         System.out.println("Welcome");
-        startGame.processUserInput().processUserInput();
-        startGame.processUserInput();
-
-
-
-        //malthes gamle noter.
-       /* while (true) {
-            Scanner input = new Scanner(System.in);
-            String userInput = input.next();
-            input.useDelimiter("\n");
-            boolean result = startGame.processUserInput(userInput);
-            if (result) {
-                //user input OK.
-            } else {
-                //User input not OK; retry
-            }
-            startGame.userOptionsForDirections();
-        }*/
+        startGame.userInterface.processUserInput();
     }
 }

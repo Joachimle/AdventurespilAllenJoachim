0070f8a05efb77469e3b8741f2e2317af1463942
Joachimle <joachimelgaard@MacBook-Air-tilhrende-Joachim.local>
2024-03-07 15:06:15 +0100

    Removed old comments

--- Player.java
+++ Player.java
@@ -64,64 +64,8 @@ public class Player {
 
             default:
                 System.out.println(" Invalid direction. Try another one");
+
+
         }
     }
-
 }
-
-/*
-public void move(String direction) {
-
-    switch (direction) {
-
-        case "go north":
-            if (player.map.currentRoom.getNorth() != null) {
-                System.out.println("please write a direction");
-                player.map.currentRoom = player.map.currentRoom.getNorth();
-                System.out.println("Going North");
-            } else {
-                System.out.println("You can't go this way");
-            }
-            break;
-
-
-        case "go west":
-            if (this.map.currentRoom.getWest() != null) {
-                System.out.println("please write a direction");
-                player.map.currentRoom = player.map.currentRoom.getWest();
-                System.out.println("Going North");
-            } else {
-                System.out.println("You can't go this way");
-            }
-            break;
-
-
-        case "go south":
-            if (this.map.currentRoom.getSouth() != null) {
-                System.out.println("please write a direction");
-                player.map.currentRoom = player.map.currentRoom.getSouth();
-                System.out.println("going south");
-            } else {
-                System.out.println("You can't go this way");
-            }
-            break;
-
-
-        case "go east":
-            if (this.map.currentRoom.getEast() != null) {
-                System.out.println("please write a direction");
-                player.map.currentRoom = player.map.currentRoom.getEast();
-                System.out.println("Going East");
-            } else {
-                System.out.println("You can't go this way");
-            }
-            break;
-
-        default:
-            System.out.println(" Invalid direction. Try another one");
-    }
-}
-public Map getCurrentroom(){
-    return currentRoom;
-}
-*/

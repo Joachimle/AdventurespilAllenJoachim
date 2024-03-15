8e1a47497e84f266e97f5e4ad28da8ee48239bc7
Joachimle <joel0002@stud.kea.dk>
2024-03-15 13:52:22 +0100

    Små justeringer

--- Food.java
+++ Food.java
@@ -8,12 +8,6 @@ public class Food extends Item { // den klasse er en subklasse af Item.mItem con
 super(shortName, longName); // kalder item kontruktor ind
         this.healthPoints = Integer.parseInt(healthpoints); // burger parseint fordi healthPoins kan blive sat til et negativ tal.
     }
-    //Food objekter
-
-//    Food mango = new Food(" Mango", " juicy & revitalising", "10");
-//    Food burger= new Food(" Burger", " Delicious beef burger", "50");
-//    Food potatoChips = new Food(" PotatoChips", " toxic crispy chips", "-10");
-
 
     //getter metod
     public int getHealthPoints(){

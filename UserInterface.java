cba9d0e7be35d1bdc88cc8462d27e100b0bf851d
Joachimle <joel0002@stud.kea.dk>
2024-03-16 10:04:12 +0100

    Delt metoden "userOptionsForDirections" op i en "userStartMenu" metode og en "userHelp" metode og flere små justeringer

--- UserInterface.java
+++ UserInterface.java
@@ -18,7 +18,7 @@ public class UserInterface {
    // Methods
     public void playGame(){
         System.out.println("Welcome");
-        userOptionsForDirections();
+        userStartMenu();
 
         while (gameIsRunning) {
             command = "";
@@ -70,14 +70,14 @@ public class UserInterface {
                     System.exit(0);
                 }
                 case "help", "h" -> {
-                    System.out.println("Please write a direction if you wanna leave the room");
+                    userHelp();
                 }
                 case "look", "l" -> {
                     System.out.println(controller.look());
-                    System.out.println("Please write a direction if you wanna leave the room");
                 }
                 case "health", "hp" -> {
-                    System.out.println(controller.getGamePlayer().getPlayerHealth());
+                    System.out.println("You currently have " + controller.getGamePlayer().getPlayerHealth() + " HP");
+                    System.out.println("To increase your HP, try to eat or drink something");
                 }
                 case "inventory", "inv", "i" -> {
                     if (controller.getGamePlayer().getInventory().isEmpty()) {
@@ -86,22 +86,30 @@ public class UserInterface {
                         System.out.println(controller.getGamePlayer().getInventory());
                 }
 
-                default -> System.out.println("Your input is invalid");
+                default -> System.out.println("Your input is invalid - try writing something else");
             }
         }
     }
 
-    public void userOptionsForDirections() {
+    public void userStartMenu() {
         System.out.println("Write 'go north' to go north");
         System.out.println("Write 'go west' to go west");
         System.out.println("Write 'go south' to go south");
         System.out.println("Write 'go east' to go east");
-        System.out.println("Write 'take' followed by the name of an item to take it");
-        System.out.println("Write 'Look' to go look around");
+        System.out.println("Write 'Look' to go look around you");
         System.out.println("Write 'Help' to ask for help");
+    }
+    public void userHelp(){
+        System.out.println("Write 'Look' to go look around you");
+        System.out.println("Write 'go' followed by a direction to move");
+        System.out.println("Write 'take' followed by the name of an item to put it in your inventory");
         System.out.println("Write 'Inventory' to look up your inventory");
+        System.out.println("Write 'eat' or 'drink' followed by the name of a food or drink in your inventory you'd like to consume");
+        System.out.println("Write 'health' to see your current health points");
     }
 
+    //Split-method bruges til at dele kommandoen til switch-casen op i to dele -
+    // - så spilleren f.eks. kan skrive "take example" for at tilgå "take" i switch og derefter videresende "example" til take-metoden i Player
     public String processUserInput(String command){
         String[] userInputArray = command.split(" ");
         this.command = userInputArray[0];

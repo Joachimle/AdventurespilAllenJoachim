1764b5d997ed4dbd80db50bffeb23aa81592b723
Joachimle <joachimelgaard@MacBook-Air-tilhrende-Joachim.local>
2024-03-19 14:45:13 +0100

    test

--- Main.java
+++ Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         UserInterface UI = new UserInterface();
         UI.playGame();
-        System.out.println("hi du der");
+        System.out.println("hi du der 2.0");
     }
 }

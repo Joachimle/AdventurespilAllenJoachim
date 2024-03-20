d3936fa63942d03cc8c240c236210907c0620f4c
Joachimle <joel0002@stud.kea.dk>
2024-03-20 13:20:29 +0100

    Test

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         UserInterface UI = new UserInterface();
         UI.playGame();
+        System.out.println("Hej");
     }
 }
